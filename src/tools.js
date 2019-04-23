b2eefb6fdd5e0c3fbe24ee538fc162e3def18ffe
Odintsov Sergey <loveravel@yandex.ru>
2019-04-24 02:59:07 +0400

    Удалил функции для рандома

--- src/tools.js
+++ src/tools.js
@@ -1,19 +1,9 @@
-/* Функция для генерирования случайного числа от min до max */
-export const getRandomInteger = (minimumNumber, maximumNumber) => {
-  let randomNumber = minimumNumber + Math.random() * (maximumNumber - minimumNumber + 1);
-  randomNumber = Math.floor(randomNumber);
-  return randomNumber;
-};
-
 export const createElement = (template) => {
   const newElement = document.createElement(`div`);
   newElement.innerHTML = template;
   return newElement.firstChild;
 };
 
-export const getRandomFromArray = (array) => {
-  return array[getRandomInteger(0, array.length - 1)];
-};
 
 export const getTimeFromMinutes = (startMinutes) => {
   return {
