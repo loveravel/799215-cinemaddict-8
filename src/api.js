a4435549f0d2c9a6968739158e3e94b498eb5902
Odintsov Sergey <loveravel@yandex.ru>
2019-04-24 23:40:01 +0400

    FIX: Trailing spaces not allowed

--- src/api.js
+++ src/api.js
@@ -9,7 +9,6 @@ const checkStatus = (response) => {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
-  
   throw new Error(`${response.status}: ${response.statusText}`);
 };
 
